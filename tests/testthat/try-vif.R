05fe1bf307a010f987ddf85ef8678e81f0b3fbd5
Michal Bojanowski <michal2992@gmail.com>
2021-01-22 23:48:08 +0100

    VIF tinkering

---
+++ tests/testthat/try-vif.R
@@ -0,0 +1,28 @@
+f <- function(e) {
+  o <- capture.output(e)
+  cat(paste0("## ", o), sep="\n")
+}
+
+
+
+data("faux.mesa.high")
+frm <- faux.mesa.high ~ nodefactor("Grade") + nodefactor("Race") +
+  nodefactor("Sex") + nodematch("Grade",diff=TRUE) +
+  nodematch("Race",diff=TRUE) + nodematch("Sex",diff=FALSE) + edges
+frm.full <- faux.mesa.high ~ edges + nodefactor("Grade") + nodefactor("Race") +
+  nodefactor("Sex") + nodematch("Grade",diff=TRUE) +
+  nodematch("Race",diff=TRUE) + nodematch("Sex",diff=FALSE) +
+  gwdegree(1.0,fixed=TRUE) + gwesp(1.0,fixed=TRUE) + gwdsp(1.0,fixed=TRUE) 
+fit <- ergm(frm)
+em <- ergm_model(fit$formula, nw = fit$network)
+str(em$terms)
+em.full <- ergm_model(frm.full, nw = faux.mesa.high)
+str(em.full$terms)
+term_names <- structure(
+  lapply(em$terms, function(x) list(coef.names = x$coef.names, )),
+  names = vapply(em$terms, "[[", character(1), "name")
+)
+to_drop <- lapply()
+
+
+R <- cov2cor(vcov(fit))
