f53f573565894c1dafafb9c435804eed52efc85e
Michal Bojanowski <michal2992@gmail.com>
2021-03-02 03:21:56 +0100

    Add naive term labeler and update vif_ergm_new()

--- R/vif_ergm.R
+++ R/vif_ergm.R
@@ -43,47 +43,54 @@ vif_ergm <- function(object, ...){
 
 
 
-# Unique model term name within a model
-# trm = element of ergm_model$terms
-.term_name <- function(trm) {
-  if(length(trm$coef.names) == 1) return(trm$coef.names)
-  l <- strsplit(trm$coef.names, "\\.")
-  paste(Reduce(intersect, l), collapse=".")
+# Unique model term labels
+# 
+# object = ergm_model
+.term_labels <- function(object) {
+  stopifnot(inherits(object, "ergm_model"))
+  # Just deparse the term calls as used in the model formula
+  vapply(object$terms, function(x) deparse(x$call), character(1))
 }
 
-vif_ergm_new <- function(object, drop_terms = NULL, drop_coef = "edges") {
+
+
+vif_ergm_new <- function(object, drop_terms = NULL, drop_coef = NULL) {
   stopifnot(inherits(object, "ergm"))
   
+  # Create `ergm_model` object to query the terms
   em <- ergm_model(object$formula, nw = object$network)
-  if(length(em$terms) < 2) stop("model contains fewer than 2 terms")
-  
-  # DF of terms with names and coef.names
+
+  # DF of terms with names, labels, and coef.names
+  coefs_per_term <- vapply(em$terms, function(k) length(k$coef.names), integer(1))
   term_db <- data.frame(
-    name = vapply(em$terms, "[[", character(1), "name"),
-    term_name = vapply(em$terms, .term_name, character(1)),
-    coef.names = lapply(em$terms, "[[", "coef.names"),
+    name = rep(vapply(em$terms, "[[", character(1), "name"), coefs_per_term),
+    term_label =rep(.term_labels(em), coefs_per_term),
+    coef_name = unlist(lapply(em$terms, "[[", "coef.names")),
     stringsAsFactors = FALSE
   )
 
+  # Get vcov matrix
   v <- vcov(object)
 
-  # Coef names to be dropped
+  # Which rows/cols of the vcov matrix should be dropped?
+  # Coefs:
   if(!is.null(drop_coef)) {
-    bad_coef_name <- !(drop_coef %in% em$coef.names)
+    bad_coef_name <- !(drop_coef %in% term_db$coef_name)
     if(any(bad_coef_name))
       stop("coefs not in the model: ", 
            paste(drop_coef[bad_coef_name], collapse=", "))
   }
-  # Get coef names of terms to be dropped
+  # Term labels (expecting deparsed formula summands):
   if(!is.null(drop_terms)) {
-    i.term <- match(drop_terms, term_db$term_name)
-    bad_term_name <- is.na(i.term)
-    if(any(bad_term_name)) 
+    bad_term_label <- !(drop_terms %in% term_db$term_label)
+    if(any(bad_term_label)) 
       stop("terms not in the model: ", 
-           paste(drop_terms[bad_term_name], collapse=", "), "\n", 
-           "Available term names: ", paste(term_db$term_name, collapse=", "))
-    drop_coef <- c(drop_coef, unlist(term_db$coef.names[i.term]))
+           paste(drop_terms[bad_term_label], collapse=", "), "\n", 
+           "Terms in the model: ", paste(term_db$term_label, collapse=", "))
+    # We will drop union of drop_coefs and coefs corresponding to drop_terms
+    drop_coef <- unique(c(drop_coef, term_db$coef_name[term_db$term_label %in% drop_terms]))
   }
+  # Drop selected rowcols from vcov
   if(!is.null(drop_coef)) {
     i <- match(drop_coef, colnames(v))
     v <- v[-i, -i]
@@ -92,7 +99,7 @@ vif_ergm_new <- function(object, drop_terms = NULL, drop_coef = "edges") {
   R <- cov2cor(v)
   detR <- det(R)
   
-  # Per coefficient
+  # Per-coefficient VIFs
   vifs <- numeric(ncol(R))
   vifs_df <- rep(1, ncol(R))
   for(i in seq(along=colnames(R))) {
@@ -101,10 +108,16 @@ vif_ergm_new <- function(object, drop_terms = NULL, drop_coef = "edges") {
     vifs[i] <- 1 / (1 - t(gvec) %*% xcor %*% gvec)
   }
   
+  # TODO: Per-term VIFs
+  
+  # Return
   structure(
     list(
-      vif = vifs,
-      vif_df = vifs_df
+      vif_coef = data.frame(
+        name = rownames(R),
+        VIF = vifs,
+        df = vifs_df
+      )
     ),
     class = "ergm_vif"
   )
--- tests/testthat/try-vif.R
+++ tests/testthat/try-vif.R
@@ -8,10 +8,10 @@ f <- function(e) {
 data("faux.mesa.high")
 frm <- faux.mesa.high ~ nodefactor("Grade") + nodefactor("Race") +
   nodefactor("Sex") + nodematch("Grade",diff=TRUE) +
-  nodematch("Race",diff=TRUE) + nodematch("Sex",diff=FALSE) + edges
+  nodematch("Race",diff=TRUE, levels=-c(1,4)) + nodematch("Sex",diff=FALSE) + edges
 frm.full <- faux.mesa.high ~ edges + nodefactor("Grade") + nodefactor("Race") +
   nodefactor("Sex") + nodematch("Grade",diff=TRUE) +
-  nodematch("Race",diff=TRUE) + nodematch("Sex",diff=FALSE) +
+  nodematch("Race",diff=TRUE, keep=-c(1,4)) + nodematch("Sex",diff=FALSE) +
   gwdegree(1.0,fixed=TRUE) + gwesp(1.0,fixed=TRUE) + gwdsp(1.0,fixed=TRUE) 
 fit <- ergm(frm)
 em <- ergm_model(fit$formula, nw = fit$network)
@@ -26,3 +26,7 @@ to_drop <- lapply()
 
 
 R <- cov2cor(vcov(fit))
+
+vif_ergm_new(fit)
+vif_ergm_new(fit, drop_terms = "edges")
+
